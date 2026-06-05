522f217c3d358e1a7cc8c58d99507084b48f7c83
Lance Johnston <lance.johnston@jpschools.org>
2026-06-05 16:25:06 +0000

    saving progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -63,7 +63,10 @@ $(document).ready(function () {
   }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat){
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
   
@@ -74,12 +77,14 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
+    currentShape = dataShapes[currentIndex]
     handleStatic(currentShape)
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex]
+    handleGood
   }
 
   function badDisplay() {
